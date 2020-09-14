3d50b89c852d1a53ad76ad94f8a57efcf91045c0
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-14 20:20:45 +0000

    added head function to get 1st element of array

---
+++ head.js
@@ -0,0 +1,17 @@
+//prettier-ignore
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌Assertion failed: ${actual} === ${expected}`);
+  }
+};
+//TEST CODE
+
+const head = function (array) {
+  return array[0];
+};
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), undefined);
