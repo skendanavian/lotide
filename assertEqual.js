fc7ec8bab2e7cbc33ec85e42bd4e3156d5325a14
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-14 20:11:28 +0000

    added template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 //prettier-ignore
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("❌Assertion Failed: " + actual + " !== " + expected);
+    console.log(`❌Assertion failed: ${actual} === ${expected}`);
   }
 };
 //TEST CODE
