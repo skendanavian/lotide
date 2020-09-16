6ee91f14655e9fcfbb6282a42dec5c2fb457e92c
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-16 19:03:06 +0000

    added countOnly.js to lotide project

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,12 @@
-//prettier-ignore
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`✅Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`❌Assertion failed: ${actual} === ${expected}`);
+    console.log(`❌❌❌Assertion failed: ${actual} === ${expected}`);
   }
 };
-//TEST CODE
+
 
 assertEqual(1, 1);
 assertEqual(1, 5);
