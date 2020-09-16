19eec443fe40b60679e355f0d4fb4c6b438e5f01
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-16 00:12:20 +0000

    edited assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+
+
+
+const eqArrays = function(array1, array2) {
+  let arrayCheck = true;
+
+  if (array1.length !== array2.length) {
+    arrayCheck = false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        arrayCheck = false;
+      }
+    }
+  }
+  return arrayCheck;
+};
+
+const assertArraysEqual = function(inputArray1, inputArray2) {
+  if (eqArrays(inputArray1, inputArray2)) {
+    console.log("✅ ✅ ✅ Assertion Passed: The arrays are identical.");
+  } else {
+    console.log("❌❌❌ Assertion failed: The arrays are not identical.");
+  }
+};
+
+
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 5, 3], [1, 2, 3]);
