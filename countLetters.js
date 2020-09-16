c7a99f2b40d8411e9cfc680f034d535ec640da1a
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-16 19:28:12 +0000

    added countLetters.js to lotide project

---
+++ countLetters.js
@@ -0,0 +1,35 @@
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌❌❌ Assertion failed: ${actual} === ${expected}`);
+//   }
+// };
+
+
+//take in sentence as a string
+//return a count of each of the letters in the string
+
+const countLetters = function(sentence) {
+  const letterCount = {};
+  for (const char of sentence) {
+    if (char !== " ") {
+      if (letterCount[char]) {
+        letterCount[char] += 1;
+      } else {
+        letterCount[char] = 1;
+      }
+    }
+  }
+
+  console.log(letterCount);
+  return letterCount;
+};
+
+
+
+countLetters("lighthouse in the house");
+
+
+
+
