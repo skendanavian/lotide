5e79c6e07f16250f2f418c76573ea384bab42fdf
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-17 20:44:26 +0000

    add findKey.js to lotide project

---
+++ findkey.js
@@ -0,0 +1,59 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌Assertion failed: ${actual} === ${expected}`);
+  }
+};
+
+// takes in object and a callback.
+//Scans the object and returns the first key for which the callback
+//returns a truthy value. if no key - return undefined.
+
+const findKey = function(object, callback) {
+  for (const item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+};
+
+
+
+findKey({
+  "Blue Hill": {stars: 1},
+  "Akaleri": {stars: 3},
+  "noma": {stars: 2},
+  "elBulli": {stars: 3},
+  "Ora": {stars: 2},
+  "Akelarre": {stars: 3}
+}, x => x.stars === 2); // => "noma"
+
+
+//TESTS
+assertEqual(findKey({
+  "Blue Hill": {stars: 1},
+  "Akaleri": {stars: 3},
+  "noma": {stars: 2},
+  "elBulli": {stars: 3},
+  "Ora": {stars: 2},
+  "Akelarre": {stars: 5}
+}, x => x.stars === 5), "Akelarre"); //True
+
+assertEqual(findKey({
+  "Blue Hill": {stars: 1},
+  "Akaleri": {stars: 2},
+  "noma": {stars: 2},
+  "elBulli": {stars: 3},
+  "Ora": {stars: 2},
+  "Akelarre": {stars: 3}
+}, x => x.stars === 2), "noma"); //False
+
+assertEqual(findKey({
+  "Blue Hill": {stars: 1},
+  "Akaleri": {stars: 3},
+  "noma": {stars: 2},
+  "elBulli": {stars: 3},
+  "Ora": {stars: 2},
+  "Akelarre": {stars: 3}
+}, x => x.stars === 2), "noma"); //True
