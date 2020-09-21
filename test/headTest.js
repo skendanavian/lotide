efaa566611df47cd5a3a9562c327f70400ff6adb
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-21 16:55:35 +0000

    added npm json, mocha and chai and reformatted tests for head.js

--- test/headTest.js
+++ test/headTest.js
@@ -1,7 +1,29 @@
-const assertEqual = require("../assertEqual.js");
-const head = require("../head.js");
+const assert = require("chai").assert;
+const head = require("../head");
+
+describe("#head", () => {
+
+  it("returns 5 for [1, 2, 3,]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), "Hello");
+  });
+
+  it("returns undefined when given and empty array []", () => {
+
+    assert.strictEqual(head([]), undefined);
+  });
+});
 
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
+
+
+
