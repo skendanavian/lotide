86a19f638a5ea4492e5bfd726f9830ab5685ca8d
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-21 17:22:17 +0000

    added mocha/chai tests for tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,34 @@
-const assertEqual = require("../assertEqual.js")
+const assert = require("chai").assert;
 const tail = require("../tail.js")
 
-const words = tail(["Yo Yo", "Lighthouse", "Labs"]);
-assertEqual(words.length, 2);
-assertEqual(words[0], "Lighthouse");
-assertEqual(words[1], "Labs");
+const test1 = ['I', 'am', 'a', 'lighthouse'];
+const test2 = [];
+const test3 = ['hello'];
+
+describe("#tail", () => {
+
+  it(`should return ['am', 'a', 'lighthouse'] given [${test1}]`, () => {
+
+    assert.deepEqual(tail(test1), ['am', 'a', 'lighthouse']);
+
+  });
+
+  it(`should return an empty array when given an empty array []`, () => {
+
+    assert.deepEqual(tail(test2), []);
+
+  });
+
+  it(`should return an empty array when given [${test3}]`, () => {
+
+    assert.deepEqual(tail(test3), []);
+
+  });
+});
+
+
+
+
+// assertEqual(words.length, 2);
+// assertEqual(words[0], "Lighthouse");
+// assertEqual(words[1], "Labs");
