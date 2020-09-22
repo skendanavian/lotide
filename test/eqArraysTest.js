50d6a59ad408094116279f1382f8951dd785dff3
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-22 00:16:47 +0000

    refactored eqArrays tests

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,24 @@
-const eqArrays = require('../eqArrays')
-const assertEqual = require('../assertEqual')
+const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
+
+
+
+describe('#eqArrays', () => {
+
+  it('should return true when given [1,2,3] and [1,2,3]', () => {
+    assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
+  })
+  it('should return true when given [] and []', () => {
+    assert.isTrue(eqArrays([], []));
+  })
+
+  it('should return false when given [1,2,3,4,5] and [1,2,3]', () => {
+    assert.isFalse(eqArrays([1, 2, 3, 4, 5], [1, 2, 3]));
+  })
+
+})
 
 //tests
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 5, 3], [1, 2, 3]), false); // => should PASS
+// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+// assertEqual(eqArrays([1, 5, 3], [1, 2, 3]), false); // => should PASS
