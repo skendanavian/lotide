2cbd203ae45fc04b5ade3fe51e711563649f2532
Soren Nissen <s.nissen.bass@gmail.com>
2020-09-22 00:25:06 +0000

    refactor eqObjects tests

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -7,18 +7,14 @@ describe('#eqArrays', () => {
 
   it('should return true when given [1,2,3] and [1,2,3]', () => {
     assert.isTrue(eqArrays([1, 2, 3], [1, 2, 3]));
-  })
+  });
   it('should return true when given [] and []', () => {
     assert.isTrue(eqArrays([], []));
-  })
+  });
 
   it('should return false when given [1,2,3,4,5] and [1,2,3]', () => {
     assert.isFalse(eqArrays([1, 2, 3, 4, 5], [1, 2, 3]));
-  })
+  });
 
-})
+});
 
-//tests
-// assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
-// assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// assertEqual(eqArrays([1, 5, 3], [1, 2, 3]), false); // => should PASS
---
+++ test/eqObjectsTest.js
@@ -0,0 +1,43 @@
+const eqObjects = require('../eqObjects');
+const assert = require('chai').assert;
+
+describe('#eqArrays', () => {
+
+  it('should return true when given {} and {}', () => {
+    assert.isTrue(eqObjects({}, {}));
+  });
+  it("should return true when given {} and {name: 'jim'}", () => {
+    assert.isFalse(eqObjects({}, {name: "jim"}));
+  });
+
+  it('should return false when given {name: "mike", age: "ten"} and {name: "mike", age: "ten"}', () => {
+    assert.isTrue(eqObjects({name: "mike", age: "ten"}, {name: "mike", age: "ten"}));
+  });
+
+});
+
+
+
+
+
+
+//Test Cases
+// const ab = {a: "1", b: "2", };
+// const ba = {b: "2", a: "1"};
+// const abc = {a: "1", b: "2", c: "3"};
+// const cba = {c: "3", b: "2", a: "1"};
+// const cbaWrong = {c: "2", b: "2", a: "1"};
+
+
+// assertEqual(eqObjects(ab, ba), true);
+// assertEqual(eqObjects(ab, abc), false);
+// assertEqual(eqObjects(cba, abc), true);
+// assertEqual(eqObjects(cbaWrong, abc), false);
+
+
+// const cd = {c: "1", d: ["2", 3]};
+// const dc = {d: ["2", 3], c: "1"};
+// assertEqual(eqObjects(cd, dc), true);
+
+// const cd2 = {c: "1", d: ["2", 3, 4]};
+// assertEqual(eqObjects(cd, cd2), false);
